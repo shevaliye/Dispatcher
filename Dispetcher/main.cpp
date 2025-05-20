2454ab2de5c79db9d934f7f97cabdb2cc42603b4
shevaliye <egkomarov@edu.hse.ru>
2025-05-20 17:04:21 +0300

    One change

--- Dispetcher/main.cpp
+++ Dispetcher/main.cpp
@@ -2,7 +2,7 @@
 #include "level.h"
 int main()
 {
-	cout << "Input parametres: resource_capacity | number_of_recources | number_of_proccesses" << endl;;
+	cout << "Input parametres: resource_capacity | number_of_recources | number_of_proccesses" << endl;
 	int resource_capacity, number_of_recources, number_of_proccesses;
 	cin >> resource_capacity>> number_of_recources>> number_of_proccesses;
 	level(resource_capacity, number_of_recources, number_of_proccesses);
